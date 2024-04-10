11480f32e6deaad894806984777bf14b533d3356
MoBsTe <valery.lobach@gmail.com>
2024-04-11 00:16:51 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="containet">
+    <h2>ToDoList</h2>
+    <div class="tte">
+      <input v-model="value" class="input_text" type="text" placeholder="Title..." />
+      <button @click="sendData()">Add</button>
+    </div>
+    <div class="list_containet">
+      <div class="list_items">
+        <div v-if="insertData.length == 1">1 element</div>
+        <TodoItem
+          v-for="(el, index) in insertData"
+          :delateData="delateData"
+          :key="index"
+          :mess="el"
+          :index="index"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TodoItem from "./components/TodoItem.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { TodoItem },
+  data() {
+    return {
+      insertData: [],
+      value: "",
+    };
+  },
+  methods: {
+    sendData() {
+      if (this.value === "") return;
+      this.insertData.push({
+        mes: this.value,
+      });
+      this.value = "";
+    },
+    delateData(index) {
+      this.insertData.splice(index, 1);
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="list_item">
+    {{ mess.mes }}
+    <div class="closet" @click="delateData(index)">X</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    mess: {
+      type: Object,
+      required: true,
+    },
+    delateData: {
+      type: Function,
+      required: true,
+    },
+    index: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style></style>
